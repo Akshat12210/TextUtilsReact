8656005390fc627a191207f5500f35d12d67c806
Akshat Modani <akshat12210@gmail.com>
2021-08-30 00:19:20 +0530

    router

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { useState } from 'react';
 import Navbar from './Components/Navbar';
 import Alert from './Components/Alert';
 import TextForm from './Components/TextForm';
-// import About from './Components/About';
+import About from './Components/About';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 export default function App() {
@@ -29,21 +36,32 @@ export default function App() {
       setmodeText("Disable Dark Mode");
       document.body.style.background='#6c757d';
       showAlert("dark mode has been enabled","success");
+      document.title= "TextUtils - Dark Mode";
     }
     else{
       setMode('light');
       setmodeText("Enable Dark Mode");
       document.body.style.background='white';
       showAlert("light mode has been enabled","success");
+      document.title= "TextUtils";
     }
   } 
   return (
     <>
+    <Router>
       <Navbar title="TextUtils" mode={mode} toggle={toggleMode} modeText={modeText}/>
       {/* <Navbar /> */}
+      <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+          <TextForm  heading="Enter the text to Analyze below" mode={mode} showAlert={showAlert}/>
+          </Route>
+        </Switch>
+        </Router>  
       <Alert alert={alert}/>
       <div className="container my-3">
-      <TextForm  heading="Enter the text to Analyze below" mode={mode} showAlert={showAlert}/>
        {/* <About />  */}
       </div>
       
